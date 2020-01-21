e25acb3d48cd2606c9e1ed6c03236d2f5769ae31
Gregory Tang <gtang159@gmail.com>
2020-01-21 15:49:48 -0800

    Added logos

--- app/src/components/CompanyInfo/CompanyInfo.js
+++ app/src/components/CompanyInfo/CompanyInfo.js
@@ -5,9 +5,30 @@ function CompanyInfo(props) {
   return (
     <div className="company-info">
       <div className="name">{props.name}</div>
-      <div className="industry">{props.industry}</div>
-      <div className="location">{props.location}</div>
-      <div className="employees">From {props.employees} employees</div>
+      <div className="info-wrap">
+        <img
+          src={require("../../images/industry.png")}
+          alt="industry"
+          className="industry-img"
+        />
+        <div className="industry">{props.industry}</div>
+      </div>
+      <div className="info-wrap">
+        <img
+          src={require("../../images/location.png")}
+          alt="location"
+          className="location-img"
+        />
+        <div className="location">{props.location}</div>
+      </div>
+      <div className="info-wrap">
+        <img
+          src={require("../../images/employee.png")}
+          alt="employee"
+          className="employee-img"
+        />
+        <div className="employees">From {props.employees} employees</div>
+      </div>
     </div>
   );
 }
--- app/src/components/ReactBlock/ReactBlock.js
+++ app/src/components/ReactBlock/ReactBlock.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./ReactBlock.scss";
 
 function ReactBlock() {
-  return <div className="react-block"></div>;
+  return (
+    <div className="react-block">
+      <img
+        src={require("../../images/react.png")}
+        alt="react"
+        className="react-logo"
+      />
+    </div>
+  );
 }
 
 export default ReactBlock;
