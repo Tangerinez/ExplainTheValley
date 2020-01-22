77968db474180264ad4be930e7f4b15e056f0baa
Gregory Tang <gtang159@gmail.com>
2020-01-21 16:04:55 -0800

    Modularized card component

--- app/src/App.js
+++ app/src/App.js
@@ -1,52 +1,20 @@
 import React from "react";
 import "./App.scss";
 import Card from "./components/Card/Card";
+import companyInfo from "./info";
 
 function App() {
   return (
     <div className="App">
-      <Card
-        name="Company1"
-        industry="SaaS/Cloud Services"
-        location="Paris"
-        employees="15 to 50"
-        jobs="8"
-      />
-      <Card
-        name="Company2"
-        industry="Artificial Intelligence/Machine Learning, SaaS.."
-        location="Clichy"
-        employees="15 to 50"
-        jobs="0"
-      />
-      <Card
-        name="Company3"
-        industry="Digital, SaaS/Cloud Services"
-        location="Paris, Lebege, Lille"
-        employees="50 to 250"
-        jobs="18"
-      />
-      <Card
-        name="Company4"
-        industry="E-commerce, IT/Digital, SaaS/Cloud Services"
-        location="Toulose"
-        employees="15 to 50"
-        jobs="3"
-      />
-      <Card
-        name="Company5"
-        industry="SaaS/Cloud Services"
-        location="Barcelona"
-        employees="50 to 250"
-        jobs="0"
-      />
-      <Card
-        name="Company6"
-        industry="Home Care Services, Organization/Managem.."
-        location="Dumbo, Paris"
-        employees="15 to 50"
-        jobs="2"
-      />
+      {companyInfo.map(info => (
+        <Card
+          name={info.name}
+          industry={info.industry}
+          location={info.location}
+          employees={info.employees}
+          jobs={info.jobs}
+        />
+      ))}
     </div>
   );
 }
---
+++ app/src/info.js
@@ -0,0 +1,46 @@
+const companyInfo = [
+  {
+    name: "Company1",
+    industry: "SaaS/Cloud Services",
+    location: "Paris",
+    employees: "15 to 50",
+    jobs: "8"
+  },
+  {
+    name: "Company2",
+    industry: "Artificial Intelligence/Machine Learning, SaaS..",
+    location: "Clichy",
+    employees: "15 to 50",
+    jobs: "0"
+  },
+  {
+    name: "Company3",
+    industry: "Digital, SaaS/Cloud Services",
+    location: "Paris, Lebege, Lille",
+    employees: "50 to 250",
+    jobs: "18"
+  },
+  {
+    name: "Company4",
+    industry: "E-commerce, IT/Digital, SaaS/Cloud Services",
+    location: "Toulose",
+    employees: "15 to 50",
+    jobs: "3"
+  },
+  {
+    name: "Company5",
+    industry: "SaaS/Cloud Services",
+    location: "Barcelona",
+    employees: "50 to 250",
+    jobs: "0"
+  },
+  {
+    name: "Company6",
+    industry: "Home Care Services, Organization/Managem..",
+    location: "Dumbo, Paris",
+    employees: "15 to 50",
+    jobs: "2"
+  }
+];
+
+export default companyInfo;
